25024068799534b34d4e5718ddfdd78ef8bd83d7
Daryl <139321314+Clissold42@users.noreply.github.com>
2023-10-09 00:33:28 +0000

    testsite1

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/dbaist.jpg"
+import image from "../images/robotacademicdress.png"
 
-const imageAltText = "Adult male speaking at a podium";
+const imageAltText = "Adult male speaking at a podiumrobot academic dress";
 
 const Home = ({ name, title }) => {
   return (
