4520fb4015d83b0c3212f772898108d0ea98c4be
Daryl <139321314+Clissold42@users.noreply.github.com>
2023-10-23 01:08:40 +0000

    mod5

---
+++ src/Components/projectList.jsx
@@ -0,0 +1,28 @@
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+export const projectList = [
+  {
+    title: "Meet Choo, my AI alter ego",
+    description: " This AI image was created using DALL-E text-to-image AI. I asked it to produce a profile image of a data scientist who is a runner. Click here to see his sister Shewie.",
+    url: "https://github.com/Clissold42/images/blob/main/Shewie.png",
+  },
+  {
+    title: "Hidden Value In Unstructured Data",
+    description: "Accident Severity on Interstate Highways. This work aims to show relationships between variables that use the free text or are unstructured fields",
+    url: "https://www.kaggle.com/code/darylb/accident-severity-on-interstate-highways-by-choo",
+  },
+  {
+    title: "My Resume",
+    description: "I specialise in data and report-building for further details regarding my skills and experience, please refer to my Resume.",
+    url: "https://github.com/Clissold42/images/blob/main/DB%20Resume%20-%201%20Page.pdf",
+  },
+  {
+    title: "Azure Machine Learning",
+    description: "I regularly build models in Azure. See an example of one of the training pipelines I've built.",
+    url: "https://github.com/Clissold42/images/blob/main/pipeline.jpg",
+  },
+];
