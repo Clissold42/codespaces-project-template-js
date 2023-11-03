5fa60980862e873c1183030ee0ea75743444b0c0
Daryl <139321314+Clissold42@users.noreply.github.com>
2023-11-03 05:00:07 +0000

    updatel

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,7 +31,7 @@ const description =
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = ["Statistician", "Data Insight and Visualisations", "AI Assistant Builder"];
+const skillsList = ["Statistician", "Data Insight and Visualisations", "Classification", "Regression", "Clustering", "Natural Language Processing", "Deep Learning", "Chemist"];
 
 /**
  * Use this to give more information about what you are passionate about,
