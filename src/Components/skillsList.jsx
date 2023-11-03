a0eb7710502630fea0f4b7d9b3256644bef5facc
Daryl <139321314+Clissold42@users.noreply.github.com>
2023-11-03 05:02:32 +0000

    updatem

--- src/Components/skillsList.jsx
+++ src/Components/skillsList.jsx
@@ -2,4 +2,4 @@
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-export const skillsList = ["Statistician", "Data Insight and Visualisations", "Chemist"];
+export const skillsList = ["Statistician", "Data Insight and Visualisations", "Classification", "Regression", "Clustering", "Natural Language Processing", "Deep Learning", "Chemist"];
